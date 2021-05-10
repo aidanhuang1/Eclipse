7febe24c1e2bdadeefd52150d32f45a31ac8877f
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-05-09 21:00:23 -0400

    test

--- main/src/main/CCCArewethereyet.java
+++ main/src/main/CCCArewethereyet.java
@@ -15,7 +15,7 @@ public class CCCArewethereyet {
 				cities[i] += cities[i - 1];
 			}
 		}
-
+		
 		for (int i=0; i<5; i++) {
 			for (int j=0; j<5; j++) {
 				System.out.print(Math.abs(cities[i]-cities[j])+" ");
