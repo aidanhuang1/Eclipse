4d96b6a71932f043f73b6ac42cb9ca85be17b330
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-05-15 19:48:30 -0400

    add

---
+++ newjavaprojectguide/src/addjavaprojecttogithubmaster.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class addjavaprojecttogithubmaster {
+
+	
+	//First create a new java project in the project explorer
+	//Double click on the java project and go to Teams > Share
+	//Delete the .profile of the Eclipse folder
+	//Refresh and the error should go away
+	//Add
+}
