ee957fb860f2410cd5e473cbad28da39d0b96854
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-05-19 11:04:15 -0400

    sd

---
+++ test2/src/testclass.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class testclass {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int i = 0;
+		int[] a = {123,456,789};
+		System.out.println(a[++i]);
+		System.out.println(i);
+		
+
+	}
+
+}
