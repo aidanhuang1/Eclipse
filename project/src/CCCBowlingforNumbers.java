e382e63b4c70690cd760c4159659246f0541bc9b
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-06-29 19:59:59 -0400

    s

---
+++ project/src/CCCBowlingforNumbers.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+public class CCCBowlingforNumbers {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static final int MOD = 1000000007;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int t = readInt(); //number of test cases
+		for (int i=0; i<t; i++) {
+			int n = readInt(), k = readInt(), w = readInt(); //n=number of bowling pins, k=number of bowling balls, w=width of the bowling balls
+		}
+
+	}
+	
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
