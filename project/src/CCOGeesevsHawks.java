939497b2b82995c3bb07480b7b753d9ab824506b
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-07-09 21:47:31 -0400

    s

--- project/src/CCOGeesevsHawks.java
+++ project/src/CCOGeesevsHawks.java
@@ -6,13 +6,69 @@ public class CCOGeesevsHawks {
 	static final int MOD = 1000000007;
 	
 	/*
-	 * Create a 2d array
-	 * columsn represent 
+	 * Create a 2d array nxn
+	 * columns represent the 
+	 *we start from first line first number, 
+	 *compare it to all the numbers on the next line starting from left to right
+	 * if the two numbers fit the conditions and we have a sum, we add it to the row i
+	 * dp[i][j] = Math.max(dp[i][j-1], team1+team2+dp[i-1][j
+	 * 
+	 * maybe we also keep a visited array?
+	 * greedy algorithms, create a class and use priorityqueue
+	 * 
 	 */
+	
+	public static class game implements Comparable<game>{
+		char c;
+		int points;
+		public game(char c, int points) {
+			this.c = c;
+			this.points = points;
+		}
+		@Override
+		public int compareTo(CCOGeesevsHawks.game o) {
+			// TODO Auto-generated method stub
+			return o.points-points;
+			
+		}
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		int n = readInt();
+		PriorityQueue<game> pq1 = new PriorityQueue<>();
+		PriorityQueue<game> pq2 = new PriorityQueue<>();
+		String[] temp1 = readLine().split("");
+		for (int i=0; i<n; i++) {
+			pq1.add(new game(temp1[i].charAt(0), readInt()));
+		}
+		String[] temp2 = readLine().split("");
+		for (int i=0; i<n; i++) {
+			pq2.add(new game(temp2[i].charAt(0), readInt()));
+		}
+		int[][] dp = new int[n+1][n+1];
 		
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=n; j++) {
+				
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		while(!pq2.isEmpty()) {
+//			game temp = pq2.poll();
+//			System.out.println(temp.c+" "+temp.points);
+//		}
 		
 
 	}
