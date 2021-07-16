7c66e0fc1b28b0250c9a38b6de890e936b0ae320
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-07-15 21:37:06 -0400

    s

---
+++ newjavaprojectguide/src/Eclipsekeyboardshortcuts.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class Eclipsekeyboardshortcuts {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static final int MOD = 1000000007;
+	
+	/*
+	 * Shift + Enter enter line below current line
+
+		Ctrl + Shift + Enter enter line above current line
+		
+		
+		Moving lines: You can move lines up and down by pressing ALT+UP or ALT+DOWN.
+		
+	 */
+}
--- project/src/DMOJACoinProblem.java
+++ project/src/DMOJACoinProblem.java
@@ -6,14 +6,50 @@ public class DMOJACoinProblem {
 	static final int MOD = 1000000007;
 	
 	/*
-	 * N is the number of different coins, but each store only accepts the first l coins
+	 * Here, if we processed each query every time, the code will TLE
+	 * However, if we take in all the query numbers, store them for later and access the array when needed
+	 * This saves time and since we only using a 1d dp array, we save memory
+	 * 
+	 * This problem also relates to DMOJCoinChange
 	 */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
-		
+		int n = readInt(), v = readInt();
+		int[] coins = new int[n+1];
+		for (int i=1; i<=n; i++) {
+			coins[i] = readInt();
+		}
+		int max = 0;
+		int[] c = new int[v+1];
+		int[] l = new int[v+1];
+		int[] ans = new int[v+1];
+		for (int i=1; i<=v; i++) {
+			c[i] = readInt();
+			max = Math.max(max, c[i]);
+			l[i] = readInt();
+		}
+
+		int[] dp = new int[max+1];
+		Arrays.fill(dp, Integer.MAX_VALUE/10);
+		dp[0] = 0;
+		for (int j=1; j<=n; j++) {
+			for (int k=coins[j]; k<=max; k++) {
+				dp[k] = Math.min(dp[k], dp[k-coins[j]]+1);
+			}
+			for (int k=1; k<=v; k++) {
+				if (l[k]==j) {
+					ans[k] = dp[c[k]];
+				}
+			}
+		}
+		for (int i=1; i<=v; i++) {
+			if (ans[i]==Integer.MAX_VALUE/10) System.out.println(-1);
+			else System.out.println(ans[i]);
+		}
+
 	}
+
 	static String next() throws IOException {
 		while (st == null || !st.hasMoreTokens())
 			st = new StringTokenizer(br.readLine().trim());
