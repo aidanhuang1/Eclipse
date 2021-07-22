0899815b3c03038dbf2bcb3fa61f028bb049e165
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-07-22 14:25:17 -0400

    s

--- project/src/CSESRectangleCutting.java
+++ project/src/CSESRectangleCutting.java
@@ -1,52 +1,32 @@
 import java.util.*;
 import java.io.*;
-public class CSESArrayDescription {
+public class CSESRectangleCutting {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static final int MOD = 1000000007;
 
-	//	dp[i][v] = number of ways to fill the array up to index i, if x[i] = v.
-
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		int n = readInt(), m = readInt();
-		int[] numbers = new int[n+1]; 
-		for (int i=1; i<=n; i++) {
-			numbers[i] = readInt();
-		}
-		int[][] dp = new int[n+2][m+2];
-		for (int i=1; i<=n; i++) {
-
-			for (int j=1; j<=m; j++) {
-
-				if (i==1) {
-					if (numbers[i]==0 || numbers[i]==j) {
-						dp[i][j] = 1;
-					} else {
-						dp[i][j] = 0;
+		int w = readInt(), h = readInt();
+		int[][] dp = new int[w+1][h+1];
+		for (int i=0; i<=w; i++) {
+			for (int j=0; j<=h; j++) {
+				if (i==j) {
+					dp[i][j] = 0;
+				} else {
+					dp[i][j] = Integer.MAX_VALUE;
+					for (int k=1; k<i; k++) {
+						dp[i][j] = Math.min(dp[i][j], dp[k][j]+dp[i-k][j]+1);
 					}
-				}
-				else {
-					if (numbers[i]==0 || numbers[i]==j) {
-						dp[i][j] = ((dp[i-1][j-1] + dp[i-1][j])%MOD + dp[i-1][j+1])%MOD;
-					} else {
-						dp[i][j] = 0;
+					for (int k=1; k<j; k++) {
+						dp[i][j] = Math.min(dp[i][j], dp[i][k]+dp[i][j-k]+1);
 					}
 				}
 			}
+			
 		}
-		int ans = 0;
-		for (int i=1; i<=m; i++) {
-			ans =( ans +dp[n][i]) % MOD;
-		}
-		System.out.println(ans);
-
-
-
-
-		
+		System.out.println(dp[w][h]);
 	}
-
 	static String next() throws IOException {
 		while (st == null || !st.hasMoreTokens())
 			st = new StringTokenizer(br.readLine().trim());
